11e56244184186194a569f8508d5eecb29c3a264
vmardar <sawvm@icloud.com>
2024-05-28 21:44:24 +0300

    lesson1_task1 create first task

---
+++ src/main/kotlin/Lesson_1/Lesson1_task1.kt
@@ -0,0 +1,8 @@
+package org.example.Lesson_1
+
+fun main() {
+    val countOrders: Int = 42
+    val gratitudeMessage: String = "Спасибо за покупку!"
+    println("Количество заказов: " + countOrders)
+    println(gratitudeMessage)
+}
