07939707f217e8637934106310f57ae631cb45dc
vmardar <sawvm@icloud.com>
2024-05-29 15:07:42 +0300

    lesson1_task3 first sending

---
+++ src/main/kotlin/Lesson_1/Lesson1_task3.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_1
+
+import kotlin.math.min
+
+fun main() {
+    val year : Int = 1961
+    var hour : String = "09"
+    var minute: String = "07"
+    //println(year)
+    //println(hour)
+    //println(minute)
+
+    hour = "10"
+    minute = "55"
+    print(hour + ":" + minute)
+}
