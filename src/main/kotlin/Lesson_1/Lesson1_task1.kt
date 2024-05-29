41fbc18631f273959daff3580167027545599026
vmardar <sawvm@icloud.com>
2024-05-29 09:53:25 +0300

    lesson1_task1 edit first task

--- src/main/kotlin/Lesson_1/Lesson1_task1.kt
+++ src/main/kotlin/Lesson_1/Lesson1_task1.kt
@@ -2,5 +2,5 @@ package org.example.Lesson_1
 
 fun main() {
     val countOrders: Int = 42
-    val gratitudeMessage: String = "Спасибо за покупку!"
+    val gratitudeMessage: String = "Спасибо за покупку"
 }
