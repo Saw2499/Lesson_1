a5e8a854a820d97120fccc7ce4b8504aaa7dccf0
vmardar <sawvm@icloud.com>
2024-05-30 14:48:27 +0300

    lesson2_Task1 first sending

---
+++ src/main/kotlin/Lesson_2/Lesson2_Task1.kt
@@ -0,0 +1,10 @@
+package org.example.Lesson_2
+
+fun main() {
+    val first: Float = 3f
+    val second: Float = 4f
+    val third: Float = 3f
+    val fourth: Float = 5f
+    val average: Float = (first + second + third + fourth) / 4
+    println(average)
+}
