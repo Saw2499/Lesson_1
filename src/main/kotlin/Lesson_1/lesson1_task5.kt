981adb40dfc825254aa61e0bfcfda57643107784
vmardar <sawvm@icloud.com>
2024-05-31 12:18:12 +0300

    lesson2_Task1 first sending

--- src/main/kotlin/Lesson_1/lesson1_task5.kt
+++ src/main/kotlin/Lesson_1/lesson1_task5.kt
@@ -2,8 +2,10 @@ package org.example.Lesson_1
 
 fun main() {
     val secondsOnSpace: Int = 6480
-    val minutesOnSpace: Int = (secondsOnSpace % 3600) / 60
-    val hoursOnSpace: Int = secondsOnSpace / 3600
-    val lastSeconds: Int = secondsOnSpace % 60
+    val secondsInHour: Int = 3600
+    val secondsInMinute: Int = 60
+    val minutesOnSpace: Int = (secondsOnSpace % secondsInHour) / secondsInMinute
+    val hoursOnSpace: Int = secondsOnSpace / secondsInHour
+    val lastSeconds: Int = secondsOnSpace % secondsInMinute
     println("0$hoursOnSpace:$minutesOnSpace:0$lastSeconds")
 }
