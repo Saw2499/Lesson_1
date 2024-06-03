da0fce712948355d5196134754d28ce454561c11
vmardar <sawvm@icloud.com>
2024-06-03 16:17:24 +0300

    Lesson2_Task3 second send

---
+++ src/main/kotlin/Lesson_2/Lesson2_Task4.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson_2
+
+import kotlin.math.roundToInt
+
+fun main() {
+    val crystalOreMined: Float = 7f
+    val ironOreMined: Float = 11f
+    val onePercentCrystal: Float = crystalOreMined / MAXIMUM_PERCENT
+    val onePercentIron: Float = ironOreMined / MAXIMUM_PERCENT
+    val bonusCrystalOreMined = (onePercentCrystal * BUFF_PERCENT).roundToInt()
+    val bonusIronOreMined = (onePercentIron * BUFF_PERCENT).roundToInt()
+
+    println("Бонус кристаллической руды: $bonusCrystalOreMined")
+    println("Бонус железной руды: $bonusIronOreMined")
+}
+const val BUFF_PERCENT = 20
+const val MAXIMUM_PERCENT = 100
