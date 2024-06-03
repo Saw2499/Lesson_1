f2d5e83089b05e9642780c6603a3e5c0733fdd16
vmardar <sawvm@icloud.com>
2024-06-03 15:40:18 +0300

    Lesson2_Task3 second send

--- src/main/kotlin/lesson2/Lesson2_task3.kt
+++ src/main/kotlin/lesson2/Lesson2_task3.kt
@@ -5,14 +5,14 @@ fun main() {
     val startHour = 9
     val startMinute = 39
     val minutesOnWay = 457
-    val hoursOnWay = minutesOnWay/ minutesInHour   //Сколько полных часов в пути (7)
-    val minutesAtLast = minutesOnWay - minutesInHour * hoursOnWay //Сколько в остатке минут (37)
+    val hoursOnWay = minutesOnWay/ MINUTES_IN_HOUR   //Сколько полных часов в пути (7)
+    val minutesAtLast = minutesOnWay - MINUTES_IN_HOUR * hoursOnWay //Сколько в остатке минут (37)
     val minutesOutHour = startMinute + minutesAtLast // Сколько минут в сумме исходных и в пути (76)
-    val arriveMinute = minutesOutHour - minutesInHour //Сколько минут в остатке при превышении часа (16)
-    val hourInOutOfHour = minutesOutHour / minutesInHour //Сколько часов в остатке из минут (1)
+    val arriveMinute = minutesOutHour - MINUTES_IN_HOUR //Сколько минут в остатке при превышении часа (16)
+    val hourInOutOfHour = minutesOutHour / MINUTES_IN_HOUR //Сколько часов в остатке из минут (1)
     val arriveHour = startHour + hoursOnWay + hourInOutOfHour
     println("Время прибытия: $arriveHour:$arriveMinute")
 
 }
 
-const val minutesInHour = 60
+const val MINUTES_IN_HOUR = 60
