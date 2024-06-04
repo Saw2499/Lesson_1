05cae64357e4e780ac45e50aff4ea1f4a773298b
vmardar <sawvm@icloud.com>
2024-06-04 17:06:35 +0300

    Lesson3_Task1 first send

---
+++ src/main/kotlin/Lesson_3/Lesson3_Task2.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson_3
+
+fun main() {
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val secondName = "Сергеевна"
+    var age = 20
+
+    println("Гражданка: $surname $name $secondName, $age")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("Гражданка: $surname $name $secondName, $age")
+}
