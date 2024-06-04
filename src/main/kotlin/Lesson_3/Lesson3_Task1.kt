741970de8b55f5300df97f2ac7346165941b3e19
vmardar <sawvm@icloud.com>
2024-06-04 23:49:07 +0300

    Lesson3_Task1 second send

--- src/main/kotlin/Lesson_3/Lesson3_Task1.kt
+++ src/main/kotlin/Lesson_3/Lesson3_Task1.kt
@@ -4,9 +4,9 @@ fun main() {
     var greeting = "Добрый день"
     val userName = "Пользователь"
 
-    println("$greeting,$userName!")
+    println("$greeting, $userName!")
 
     greeting = "Добрый вечер"
 
-    println("$greeting,$userName!")
+    println("$greeting, $userName!")
 }
