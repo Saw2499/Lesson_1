ce680e716f09e824e794a21f64fa7ec3151dcb36
vmardar <sawvm@icloud.com>
2024-06-05 11:22:56 +0300

    Lesson4_Task1 First send

---
+++ src/main/kotlin/Lesson_4/Lesson4_Task1.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson_4
+
+fun main () {
+    val reservedTablesToday = ALL_TABLES
+    val freeTablesTomorrow = 4
+    val reservedTablesTomorrow = ALL_TABLES - freeTablesTomorrow
+    println("Доступность столиков на сегодня: ${reservedTablesToday < ALL_TABLES}\nДоступность столиков на завтра: ${reservedTablesTomorrow < ALL_TABLES}" )
+}
+const val ALL_TABLES = 13
