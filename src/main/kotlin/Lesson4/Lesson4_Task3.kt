f7aefb651f77245feeadb606fd8a40cea25d7a98
vmardar <sawvm@icloud.com>
2024-06-07 12:17:12 +0300

    Lesson4_Task3 second send

--- src/main/kotlin/Lesson4/Lesson4_Task3.kt
+++ src/main/kotlin/Lesson4/Lesson4_Task3.kt
@@ -1,11 +1,11 @@
 package org.example.Lesson4
 
 fun main() {
-    val sunnyWeather = true
+    val isSunny = true
     val openAwning = true
     val humidity: Int = 20
     val season = "Winter"
-    val conditions = sunnyWeather == true && openAwning == true && season != "Winter" && humidity == NORMAL_HUMIDITY
+    val conditions = isSunny == true && openAwning == true && season != "Winter" && humidity == NORMAL_HUMIDITY
 
     println("Благоприятные ли условия сейчас для роста бобовых? $conditions")
 }
